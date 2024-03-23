85cfced04d2113dce2b95a088986ccd2cd3ff5a5
HanaXasro <88589183+HanaXasro@users.noreply.github.com>
2024-03-23 17:26:58 +0300

    Add PostRequestSevice and remove Folder Account in Service folder

--- app/src/main/java/com/example/cinemanabooking/Hellper/Helper.java
+++ app/src/main/java/com/example/cinemanabooking/Hellper/Helper.java
@@ -1,5 +1,5 @@
 package com.example.cinemanabooking.Services.Account;
 
-public  class Helper {
+public class Helper {
     public static final String BaseUrl = "http://192.168.1.8:5000/";
 }
--- app/src/main/java/com/example/cinemanabooking/Services/RequestServices/OtherPostServices.java
+++ app/src/main/java/com/example/cinemanabooking/Services/RequestServices/OtherPostServices.java
@@ -1,9 +1,9 @@
-package com.example.cinemanabooking.Services.Account;
+package com.example.cinemanabooking.Services.RequestServices;
 
 import android.os.AsyncTask;
 import android.util.Log;
 
-import com.example.cinemanabooking.Dtos.AccountDto.LoginResponseDto;
+import com.example.cinemanabooking.Hellper.Helper;
 import com.example.cinemanabooking.Services.ApiResponse;
 
 import org.json.JSONArray;
@@ -18,35 +18,35 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
 
-public class ForgotTask extends AsyncTask<String, Void, ApiResponse> {
-    private ForgotListener listener;
-    public ForgotTask(ForgotListener listener) {
+public class PostServices  extends AsyncTask<String, Void, ApiResponse> {
+    private PostListener listener;
+    public PostServices(PostListener listener) {
         this.listener = listener;
     }
-    private ApiResponse _apiResponse= new ApiResponse();
+    private ApiResponse _apiResponse = new ApiResponse();
     @Override
     protected ApiResponse doInBackground(String... params) {
         String Url = params[0];
-        String email = params[1];
+        String JsonPost = params[1];
 
 
         try {
-            URL url = new URL(Helper.BaseUrl+Url);
+            URL url = new URL(Helper.BaseUrl + Url);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("POST");
             urlConnection.setDoOutput(true);
             urlConnection.setRequestProperty("Content-Type", "application/json");
-            JSONObject postData = new JSONObject();
-            postData.put("email", email);
             OutputStream outputStream = urlConnection.getOutputStream();
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
-            writer.write(postData.toString());
+            writer.write(JsonPost);
             writer.flush();
             writer.close();
             outputStream.close();
+
+
             int responseCode = urlConnection.getResponseCode();
+
             if (responseCode == HttpURLConnection.HTTP_OK) {
 
                 InputStream inputStream = urlConnection.getInputStream();
@@ -58,11 +58,10 @@ public class ForgotTask extends AsyncTask<String, Void, ApiResponse> {
                 }
                 reader.close();
                 inputStream.close();
-                JSONObject JsonResponse= new JSONObject(response.toString());
-                _apiResponse.Result = JsonResponse.getString("result");
+                _apiResponse.Result=response.toString();
                 _apiResponse.IsSuccess=true;
-            }
-            else if (responseCode== HttpURLConnection.HTTP_NOT_FOUND){
+
+            } else  {
 
                 InputStream inputStream = urlConnection.getErrorStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
@@ -73,30 +72,31 @@ public class ForgotTask extends AsyncTask<String, Void, ApiResponse> {
                 }
                 reader.close();
                 inputStream.close();
-                JSONObject JsonResponse= new JSONObject(response.toString());
-                JSONArray JsonErrorMessage= JsonResponse.getJSONArray("errorMessage");
-                for(int i=0;i<JsonErrorMessage.length();i++){
+                JSONObject JsonResponse = new JSONObject(response.toString());
+                JSONArray JsonErrorMessage = JsonResponse.getJSONArray("errorMessage");
+                for (int i = 0; i < JsonErrorMessage.length(); i++) {
                     _apiResponse.ErrorMessage.add(JsonErrorMessage.getString(i));
                 }
             }
-            _apiResponse.HttpStateCode=responseCode;
-            return  _apiResponse;
+            _apiResponse.HttpStateCode = responseCode;
+            return _apiResponse;
 
-        } catch (Exception e)  {
-            Log.e("Login Request","  Error:   " + e.getMessage());
+        } catch (Exception e) {
+            Log.e("Login Request", "  Error:   " + e.getMessage());
             return null;
         }
     }
     @Override
     protected void onPostExecute(ApiResponse result) {
         if (result.IsSuccess) {
-            listener.onForgotSuccess(result);
+            listener.onPostSuccess(result);
         } else {
-            listener.onForgotFailure(result);
+            listener.onPostFailure(result);
         }
     }
-    public interface ForgotListener {
-        void onForgotSuccess(ApiResponse response);
-        void onForgotFailure(ApiResponse response);
+    public interface PostListener {
+        void onPostSuccess(ApiResponse response);
+
+        void onPostFailure(ApiResponse response);
     }
 }
