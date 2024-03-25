04fbf2158d5e3da5a0e1cbe8678d9b9dd64baf7c
Dlovan <dlovan.a6103@gmail.com>
2024-03-25 22:12:03 +0300

    changed Colors and added one page

---
+++ app/src/main/java/com/example/cinemanabooking/SignUpVerify.java
@@ -0,0 +1,14 @@
+package com.example.cinemanabooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpVerify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_verify);
+    }
+}
