48de0873f65b5be9cdf026184fed561ffb827550
HanaXasro <88589183+HanaXasro@users.noreply.github.com>
2024-04-15 08:59:05 +0300

    fix post and RemeberMe checkbox

--- app/src/main/java/com/example/cinemanabooking/Hellper/Helper.java
+++ app/src/main/java/com/example/cinemanabooking/Hellper/Helper.java
@@ -1,5 +1,5 @@
 package com.example.cinemanabooking.Hellper;
 
 public class Helper {
-    public static final String BaseUrl = "http://192.168.1.8:5000/";
+    public static final String BaseUrl = "http://192.168.174.87:5000/";
 }
