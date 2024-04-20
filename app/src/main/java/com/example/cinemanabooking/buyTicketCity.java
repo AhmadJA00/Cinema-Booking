f4a86ae0a0171523933ead4ac3fe3193c20ea994
Dlovan <dlovan.a6103@gmail.com>
2024-04-20 15:43:49 +0300

    created city page in ticket part

---
+++ app/src/main/java/com/example/cinemanabooking/buyTicketCity.java
@@ -0,0 +1,26 @@
+package com.example.cinemanabooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class buyTicketCity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_ticket_city);
+
+        //to fill first progress bar
+        ProgressBar cityProgressBar = findViewById(R.id.cityProgressBar);
+        cityProgressBar.setProgress(100);
+
+        //to start on ticket
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavbar);
+        bottomNavigationView.setSelectedItemId(R.id.ticket);
+
+    }
+}
